a98933b5ec0bd822fe4401018f42f0e623b20a99
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-04-29 22:07:50 +0100

    Update SevensOut.cs

--- CMP1903_A1_2324/SevensOut.cs
+++ CMP1903_A1_2324/SevensOut.cs
@@ -8,3 +8,29 @@ namespace CMP1903_A1_2324
 {
     internal class SevensOut
     {
+        int Total; 
+
+        public SevensOut()
+        {
+            Die1 = int Roll();
+            Die2 = int Roll();
+            Console.WriteLine($"Value of Die 1 is: {str Die1}");
+            Console.WriteLine($"Value of Die 2 is: {str Die2}");
+            if (int Die1 == int Die2)
+            {
+                Console.WriteLine($"You rolled a double!");
+                Die1 = int Die1 * 2;
+                Die2 = int Die2 * 2;
+            }
+            Total = int Die1 + int Die2;
+            Console.WriteLine($"Total of both die is: {str Total}");
+            if (int Total == 7)
+            {
+                Console.WriteLine($"You're total is 7- Game over!!");
+                Quit();
+            }
+
+        }
+
+    }
+}
