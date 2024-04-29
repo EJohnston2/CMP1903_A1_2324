dc0f73dadae1fbd3835b4269c951197363a7df8a
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-04-29 21:11:22 +0100

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -15,13 +15,23 @@ namespace CMP1903_A1_2324
         int RollValue;
         int Total;
 
-        public int Roll(Random _rand)
+        public int Roll1(Random _rand)
         { 
-            RollValue = int _rand.Next(1, 7, 1);
-            return RollValue;
-            Total = int RollValue + int Total;
+            RollValue1 = int _rand.Next(1, 7, 1);
+            return RollValue1;
+            Total = int RollValue1 + int Total;
+            return Total
+            
+        }
+
+        public int Roll2(Random _rand)
+        {
+            RollValue2 = int _rand.Next(1, 7, 1):
+            return RollValue2;
+            Total = int RollValue2 + int Total; 
             return Total
         }
+        
         //simulates a dice roll
         //produces a random value between 1 and 6
         //returns the value of the dice
