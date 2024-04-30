f6d96f62677f4c426a2fc024757b49254be8179a
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-04-30 17:51:03 +0100

    Update ThreeOrMore.cs

--- CMP1903_A1_2324/ThreeOrMore.cs
+++ CMP1903_A1_2324/ThreeOrMore.cs
@@ -8,20 +8,61 @@ namespace CMP1903_A1_2324
 {
     internal class ThreeOrMore
     {
+      int Counter; 
       
-      
-      Die1 = int Roll();
-      Die2 = int Roll();
-      Die3 = int Roll():
-      Die4 = int Roll();
-      Die5 = int Roll();
+      public ThreeOrMore()
+      {
+        Die1 = int Roll();
+        Die2 = int Roll();
+        Die3 = int Roll():
+        Die4 = int Roll();
+        Die5 = int Roll();
 
-      int[] DieValues = {Die1, Die2, Die3, Die4, Die5};
+        int[] DieValues = {Die1, Die2, Die3, Die4, Die5};
 
-      Console.WriteLine($"Your die values are: ");
-      Console.WriteLine(DieValues[]);
-      
+        Console.WriteLine($"Your die values are: ");
+        Console.WriteLine(DieValues[]);
+
+        for i in range(1, 6, 1)
+        {
+          for j in range((i+1), 6, 1)
+          {
+            if (i == j) 
+            {
+              Counter = int Counter + 1
+            }
+          }
+        }
+
+        if (Counter == 2)
+        {
+          Console.WriteLine($"You have rolled two of the same values");
+          Choice = Console.ReadLine($"Rethrow all die or rethrow the remaining three die?");
+          if (Choice == ("Rethrow all die" || "Rethrow all" || "All"))
+          {
+            NewDie1 = int Roll();
+            NewDie2 = int Roll();
+            NewDie3 = int Roll();
+            NewDie4 = int Roll();
+            NewDie5 = int Roll();
+
+            int[] NewDieValues = {NewDie1, NewDie2, NewDie3, NewDie4, NewDie5};
+            Console.WriteLine($"Your new die values are: ");
+            Console.WriteLine(NewDieValues[]);
+          }
+          if (Choice == ("Rethrow the remaining three die" || "Remaining three" || "Rethrow three remaining" || "Three" || "Remaining")
+          {
+            NewDie3 = int Roll();
+            NewDie4 = int Roll();
+            NewDie5 = int Roll();
+
+            int[] UpdatedDieValues = {Die1, Die2, NewDie3, NewDie4, NewDie5};
+            Console.WriteLine($"Your updated die values are: "};
+            Console.WriteLine(UpdatedDieValues);
+          }
+                              
+         
+        
+
     
 
-    }
-}
