3e1e254e0ea1bc75a2f6a46109ed671c7626117e
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-04-30 17:24:54 +0100

    Update ThreeOrMore.cs

--- CMP1903_A1_2324/ThreeOrMore.cs
+++ CMP1903_A1_2324/ThreeOrMore.cs
@@ -8,13 +8,20 @@ namespace CMP1903_A1_2324
 {
     internal class ThreeOrMore
     {
+      
+      
       Die1 = int Roll();
       Die2 = int Roll();
       Die3 = int Roll():
       Die4 = int Roll();
       Die5 = int Roll();
 
-      if 
+      int[] DieValues = {Die1, Die2, Die3, Die4, Die5};
+
+      Console.WriteLine($"Your die values are: ");
+      Console.WriteLine(DieValues[]);
+      
+    
 
     }
 }
