a94153de75bbbb971d306cd0adce10a82ea2e5b7
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-05-01 18:39:44 +0100

    Update Statistics.cs

--- CMP1903_A1_2324/Statistics.cs
+++ CMP1903_A1_2324/Statistics.cs
@@ -10,7 +10,19 @@ namespace CMP1903_A1_2324
     {
       public Stats()
       {
+        Console.WriteLine($"Displaying game statistics: ");
         
+        Console.WriteLine($"Seven's Out statistics: ");
+        Console.WriteLine($"Amount of games played: " + {str Game.SevensPlayed});
+        Console.WriteLine($"Number of Player 1 wins: " + {str Game.SevensPlayer1Win});
+        Console.WriteLine($"Number of Player 2 wins: " + {str Game.SevensPlayer2Win});
+        Console.WriteLine($"Number of draws: " + {str Game.SevensPlayerDraw});
+
+        Console.WriteLine($"Three or more statistics");
+        Console.WriteLine($"Amount of games played: " + {str Game.ThreePlayed});
+        Console.WriteLine($"Number of Player 1 wins: " + {str Game.ThreePlayer1Win});
+        Console.WriteLine($"Number of Player 2 wins: " + {str Game.ThreePlayer2Win});
+        Console.WriteLine($"Number of draws: " + {str Game.ThreePlayerDraw});
       }  
     }
 }
