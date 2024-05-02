5f50b233c049a699dc63540e11b578e141ab53e0
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-05-02 05:49:04 +0100

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -6,13 +6,32 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Testing
+    public class Testing
     {
-        Game();
-        
-        NewDie = Die();
-        debug.assert(NewDie > 6);
-        debug.assert(NewDie < 1);
-        
+        public static void TestDie()
+        {
+            private TestDie = new Die();
+            for (int i = 0, i < 1000; i++)
+            {
+                private int Result = Die.Roll();
+                Debug.Assert(Result >= 1 && Result <=6, "Die roll out of range");
+            }
+        }
+
+        public static void TestSeven()
+        {
+            private TestSeven = new SevensOut();
+            Debug.Assert(TestSeven.GameOver(7), "Sevens out total score error");
+            Debug.Assert(TestSeven.GameOver(9), "Sevens out total score error");
+            Debug.Assert(!TestSeven.GameOver(6), "Sevens out total score error");
+        }
+
+        public static void TestThree()
+        {
+            private TestThree = new ThreeOrMore();
+            Debug.Assert(TestThree.WinnerFound(20), "Three or more score error");
+            Debug.Assert(TestThree.WinnerFound(24), "Three or more score error");
+            Debug.Assert(!TestThree.WinnerFound(18), "Three or more score error");
+        }
     }
 }
